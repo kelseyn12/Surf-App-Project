aafeacfc53bdc998ab2969eb7bbb572b57bb6178
kelsey nocek <kelseyn12@hotmail.com>
2021-12-13 15:29:00 -0600

    created an if/else function to take into consideration the wind direction and wind speed in order to determine whether or not there is surf on the given day

--- script.js
+++ script.js
@@ -18,8 +18,10 @@ function handleGetData(evt) {
 
 //do not refresh page
 
-$.ajax(URL).then(function(data) {
-    render(data);
+$.ajax(URL).then(function(currentData) {
+ render(currentData);
+    
+    
 
     //take the ajax URL and then get data from that function.
     //render data
@@ -32,20 +34,25 @@ $.ajax(URL).then(function(data) {
 
 
 
-
-
-
-
-
-
-
-function render(data) {
+function render(currentData) {
     $('main').html(`
-        <h3>Temp: ${Math.floor(data.current.temp)}&deg;F</h3>
-        <h3>Wind Direction: ${Math.floor(data.current.wind_deg)}&deg;</h3>
-        <h3>Speed: ${Math.floor(data.current.wind_speed)} mph</h3>
+    <h2>Current</h2>
+        <h3>Temp: ${Math.floor(currentData.current.temp)}&deg;F</h3>
+        <h3>Wind Direction: ${Math.floor(currentData.current.wind_deg)}&deg;</h3>
+        <h3>Speed: ${Math.floor(currentData.current.wind_speed)} mph</h3>
     `);
-}
 
 
 
+
+if (currentData.current.wind_deg >= 0 && currentData.current.wind_deg <= 25
+    && currentData.current.wind_speed >= 28) {
+    alert ("There is surf today! It's glassy out there! Have fun!")
+
+    } else if (currentData.current.wind_deg >= 26 && currentData.current.wind_deg <= 45
+        && currentData.current.wind_speed >= 28) {
+        alert ("There is surf today, but it's choppy! Be careful! Have fun!")
+
+    } else {
+        alert ("There's no surf today. Check back tomorrow!")
+}};

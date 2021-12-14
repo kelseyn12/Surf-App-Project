01ff52fa074c86a7f82b9af3dee9df254dd6760e
kelsey nocek <kelseyn12@hotmail.com>
2021-12-14 15:00:18 -0600

    added jquery fade in and out

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 //define variables:
 
+
 const URL = 'https://api.openweathermap.org/data/2.5/onecall?lat=46.92557&lon=-91.82174&appid=88723180e3c19e3235dfaf41df75b619&units=imperial&q='
 
 const $button = $('#button');
@@ -10,6 +11,7 @@ const $button = $('#button');
 // })
 
 $button.on('click', handleGetData);
+$('p').fadeIn(2000);
 
 //when the button is clicked, get this data
 
@@ -36,25 +38,50 @@ $.ajax(URL).then(function(currentData) {
 
 function render(currentData) {
     $('p').html(`
-    <h3>Currently</h3>
+    <h3>Currently</h3><br>
         <h4>Temp: ${Math.floor(currentData.current.temp)}&deg;F</h4>
         <h4>Wind Direction: ${Math.floor(currentData.current.wind_deg)}&deg;</h4>
         <h4>Speed: ${Math.floor(currentData.current.wind_speed)} mph</h4>
     `
     )
 
-
-
 if (currentData.current.wind_deg >= 0 && currentData.current.wind_deg <= 25
     && currentData.current.wind_speed >= 28) {
-    $('main').html(`<h2>Surf Status: Surf! It's glassy! Have fun!</h2>`);
+    $('main').html(`<h1>Surf Status: Surf! It's glassy! Have fun!</h1>`);
 
     } else if (currentData.current.wind_deg >= 26 && currentData.current.wind_deg <= 45
         && currentData.current.wind_speed >= 28) {
-        $('main').html(`<h2>Surf Status: Surf! Choppy. Be careful! Have fun!</h2>`);
+        $('main').html(`<h1>Surf Status: Surf! Choppy. Be careful! Have fun!</h1>`);
 
     } else {
-        $('main').html(`<h2>Surf Status: None. Check back later!</h2>`);
+        $('main').html(`<h1>Surf Status: None. Check back later!</h1>`);
     }
 };
+
+
+//make everything in 'p' and 'main' fade in
+
+
+$("#button").click(function () {
+    $(this).fadeOut(1500, function () {
+        $(this).fadeIn(9000)
+    })
+});
+
+$('p').hide()
+$('#button').click(function () {
+    $('p').fadeIn(3000, function () {
+        $('p').fadeOut(5000)
+    })
+});
+
+$('main').hide()
+$('#button').click(function () {
+    $('main').fadeIn(3000, function () {
+        $('main').fadeOut(5000)
+    })
+});
+ 
+
+
 

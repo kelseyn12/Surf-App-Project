64b2126851cea685502461eb0e6397f5ed2fd1da
kelsey nocek <kelseyn12@hotmail.com>
2021-12-16 16:37:58 -0600

    did some more css. added animation using keyframes in css, added jquery delay and fade out on surf

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 //define variables:
 
-
+$(function(){
 const URL = 'https://api.openweathermap.org/data/2.5/onecall?lat=46.92557&lon=-91.82174&appid=88723180e3c19e3235dfaf41df75b619&units=imperial&q='
 
 const $button = $('#button');
@@ -18,20 +18,20 @@ $('p').fadeIn(2000);
 function handleGetData(evt) {
     evt.preventDefault();
 
-//do not refresh page
+    //do not refresh page
 
-$.ajax(URL).then(function(currentData) {
- render(currentData);
-    
-    
+    $.ajax(URL).then(function (currentData) {
+        render(currentData);
 
-    //take the ajax URL and then get data from that function.
-    //render data
 
-}, function(error){
-    console.log('something went wrong')
-    console.log(error);
-})   
+
+        //take the ajax URL and then get data from that function.
+        //render data
+
+    }, function (error) {
+        console.log('something went wrong')
+        console.log(error);
+    })
 }
 
 
@@ -45,16 +45,16 @@ function render(currentData) {
     `
     )
 
-if (currentData.current.wind_deg >= 0 && currentData.current.wind_deg <= 25
-    && currentData.current.wind_speed >= 28) {
-    $('main').html(`<h1>Surf Status: Surf! It's glassy! Have fun!</h1>`);
+    if (currentData.current.wind_deg >= 0 && currentData.current.wind_deg <= 25
+        && currentData.current.wind_speed >= 28) {
+        $('#head').html(`<h1>Surf Status: Surf! It's glassy! Have fun!</h1>`);
 
     } else if (currentData.current.wind_deg >= 26 && currentData.current.wind_deg <= 45
         && currentData.current.wind_speed >= 28) {
-        $('main').html(`<h1>Surf Status: Surf! Choppy. Be careful! Have fun!</h1>`);
+        $('#head').html(`<h1>Surf Status: Surf! Choppy. Be careful! Have fun!</h1>`);
 
     } else {
-        $('main').html(`<h1>Surf Status: None. Check back later!</h1>`);
+        $('#head').html(`<h1>Surf Status: None. Check back later!</h1>`);
     }
 };
 
@@ -75,13 +75,16 @@ $('#button').click(function () {
     })
 });
 
-$('main').hide()
+$('#head').hide()
 $('#button').click(function () {
-    $('main').fadeIn(3000, function () {
-        $('main').fadeOut(5000)
+    $('#head').fadeIn(3000, function () {
+        $('#head').fadeOut(5000)
     })
 });
- 
 
-
-
+$('#button').click(function () {
+    $('.content').fadeOut(1000, function () {
+        $('.content').delay(6000).fadeIn(5000)
+    })
+});
+});
